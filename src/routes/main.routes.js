88d4768117f528b43488bae280c7e4b5cdbf7a6a
JacobNA <jacob.norlinandersson@gmail.com>
2016-02-24 12:49:12 +0100

    Added routes for getting data  for specific date. Removed unneccesary prints

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -9,12 +9,24 @@ const router = express.Router();
 
 
 
+router.get('/getCountryDatas/:date', (req, res) => {
+	var db = pgp("postgres://postgres:123456@localhost:1234/postgres");
+	db.manyOrNone("select * from facebook_data_country where date=\'"+req.params.date+"\'")
+	.then(data => {
+		res.jsonp(data);
+	})
+	.catch(error => {
+		console.log(error);
+		res.send(error);
+	});
+
+});
+
+
 router.get('/getCountryData/:country', (req, res) => {
 	var db = pgp("postgres://postgres:123456@localhost:1234/postgres");
-	console.log("fetching country data");
 	db.manyOrNone("select * from facebook_data_country where country ilike \'"+req.params.country+"\'")
 	.then(data => {
-		console.log("finished fetching country data")
 		res.jsonp(data);
 	})
 	.catch(error => {
@@ -26,10 +38,8 @@ router.get('/getCountryData/:country', (req, res) => {
 
 router.get('/getCountries', (req, res) => {
 	var db = pgp("postgres://postgres:123456@localhost:1234/postgres");
-	console.log("fetching country data");
 	db.manyOrNone("select distinct country from facebook_data_country")
 	.then(data => {
-		console.log("finished fetching country data")
 		let countries = _.map(data, obj => {
 			return obj.country;
 		});
@@ -47,7 +57,6 @@ router.get('/getCountryDatas', (req, res) => {
 	var db = pgp("postgres://postgres:123456@localhost:1234/postgres");
 	db.manyOrNone("select * from facebook_data_country")
 	.then(data => {
-		console.log("finished fetching country data")
 		res.jsonp(data);
 	})
 	.catch(error => {
@@ -62,7 +71,6 @@ router.get('/getCityDatas', (req, res) => {
 	var db = pgp("postgres://postgres:123456@localhost:1234/postgres");
 	db.manyOrNone("select * from facebook_data_city")
 	.then(data => {
-		console.log("finished fetching city data")
 		res.jsonp(data);
 	})
 	.catch(error => {
@@ -102,7 +110,6 @@ router.get('/getCities', (req, res) => {
 
 router.get('/getDemographics', (req, res) => {
 	var db = pgp("postgres://postgres:123456@localhost:1234/postgres");
-	console.log("fetching country data");
 	db.manyOrNone("select distinct demographic from facebook_data_demographic")
 	.then(data => {
 		let cities = _.map(data, obj => {
@@ -119,7 +126,6 @@ router.get('/getDemographics', (req, res) => {
 
 router.get('/getDemographic/:demographic', (req, res) => {
 	var db = pgp("postgres://postgres:123456@localhost:1234/postgres");
-	console.log("fetching country data");
 	db.manyOrNone("select * from facebook_data_demographic ilike \'"+req.params.demographic+"\'")
 	.then(data => {
 		res.jsonp(data);
@@ -133,10 +139,8 @@ router.get('/getDemographic/:demographic', (req, res) => {
 
 router.get('/getDemographicDatas', (req, res) => {
 	var db = pgp("postgres://postgres:123456@localhost:1234/postgres");
-	console.log("fetching country data");
 	db.manyOrNone("select * from facebook_data_demographic")
 	.then(data => {
-		console.log("finished fetching country data")
 		res.jsonp(data);
 	})
 	.catch(error => {
